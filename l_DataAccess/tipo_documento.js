2112629ef35819d287d35d090ffc7afda9235f54
Rafaelmor2000 <Rafaelmor2000@gmail.com>
2024-04-10 01:52:43 -0600

    ajustar query

--- l_DataAccess/tipo_documento.js
+++ l_DataAccess/tipo_documento.js
@@ -4,9 +4,10 @@ module.exports = {
         pool.getConnection((err, connection) => {
             if(err) throw err
             console.log('connected as id ' + connection.threadId)
-            connection.query('SELECT * from clasificacion', (err, rows) => {
+            connection.query('SELECT tipodocumento.idTipo, tipodocumento.nombre, clasificacion.nombreClasificacion FROM tipodocumento INNER JOIN clasificacion ON tipodocumento.clasificacion = clasificacion.idClasificacion', (err, rows) => {
                 connection.release() // return the connection to pool
                 if (!err) {
+                    console.log(rows)
                     return rows
                 } else {
                     console.log(err)
