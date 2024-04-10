649e5b54175919fcab65102d58365c2e039470f3
Rafaelmor2000 <Rafaelmor2000@gmail.com>
2024-04-10 08:56:43 -0600

    lidiando con asincronía poco a poco

--- l_DataAccess/tipo_documento.js
+++ l_DataAccess/tipo_documento.js
@@ -1,18 +1,23 @@
 const {pool} = require("../Utilities/pool")
 module.exports = {
     getAllTipo_documento : () => {
-        pool.getConnection((err, connection) => {
-            if(err) throw err
-            console.log('connected as id ' + connection.threadId)
-            connection.query('SELECT tipodocumento.idTipo, tipodocumento.nombre, clasificacion.nombreClasificacion FROM tipodocumento INNER JOIN clasificacion ON tipodocumento.clasificacion = clasificacion.idClasificacion', (err, rows) => {
-                connection.release() // return the connection to pool
-                if (!err) {
-                    console.log(rows)
-                    return rows
-                } else {
-                    console.log(err)
-                }
+        const dataPromise = new Promise((resolve, reject) => {
+            pool.getConnection((err, connection) => {
+                if(err) throw err
+                console.log('connected as id ' + connection.threadId)
+                connection.query('SELECT tipodocumento.idTipo, tipodocumento.nombre, clasificacion.nombreClasificacion FROM tipodocumento INNER JOIN clasificacion ON tipodocumento.clasificacion = clasificacion.idClasificacion', (err, rows) => {
+                    connection.release() // return the connection to pool
+                    if (!err) {
+                        resolve(rows)
+                    } else {
+                        reject(console.log(err))
+                    }
+                })
             })
+        })
+        dataPromise.then(rows => {
+            console.log(rows)
+            return rows
         })
     }
 }
