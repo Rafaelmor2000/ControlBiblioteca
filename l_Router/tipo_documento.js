c9f6320a2e0eb69a66b9a1fbf370dd12aa2b5bf6
Rafaelmor2000 <Rafaelmor2000@gmail.com>
2024-04-16 23:44:33 -0600

    Limpiar separaciones entre capas de presentación, servicio y router

--- l_Router/tipo_documento.js
+++ l_Router/tipo_documento.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const tipoController = require('../l_Service/tipo_documentoController')
+const { data } = require('jquery')
 
-router.get('/', tipoController.get)
-router.get('/nuevo', tipoController.nuevo)
-router.post('/guardar', tipoController.guardar)
+router.get('/', (req, res) => {
+    let list = tipoController.get(function(list){
+        res.render("tipoDocumento", {list: list})
+    })
+})
+
+router.get('/nuevo', (req, res) => {
+    let list = tipoController.nuevo(function(list){
+        res.render("newTipoDocumento", {list: list})
+    })
+})
+
+router.post('/guardar', (req,res) => {
+    tipoController.guardar(req.body)
+    res.redirect('./')
+})
 
 
 module.exports = router
--- l_Service/tipo_documentoController.js
+++ l_Service/tipo_documentoController.js
@@ -3,31 +3,41 @@ const dataController = require('../l_DataAccess/tipo_documento')
 const tipo_documento = require('../Utilities/tipo_documento')
 const clasificacion = require('../Utilities/clasificacion')
 module.exports = {
-    get : (req, res) => {
-        dataController.getTipoDisplayList(function(json){
+    get : (callback) => {
+        const dataPromise = new Promise((resolve) => {
             let list = []
-            for (let key in json){
-                let tipo = new tipo_documento(json[key].idTipo, json[key].nombre, json[key].nombreClasificacion)
-                list.push(tipo)
-            }
-            res.render("tipoDocumento", {list: list})
+            dataController.getTipoDisplayList(function(json){
+                for (let key in json){
+                    let tipo = new tipo_documento(json[key].idTipo, json[key].nombre, json[key].nombreClasificacion)
+                    list.push(tipo)
+                }
+                resolve(list)
+            })
+        })
+        dataPromise.then(list => {
+            callback(list)
         })
     },
-    nuevo : (req, res) => {
-        dataController.getClasificaciones(function(json){
+
+    nuevo : (callback) =>{
+        const dataPromise = new Promise((resolve) => {
             let list = []
-            for (let key in json){
-                let tipo = new clasificacion(json[key].idClasificacion, json[key].nombreClasificacion)
-                list.push(tipo)
-            }
-            console.log(list)
-            res.render("newTipoDocumento", {list: list})
+            dataController.getClasificaciones(function(json){
+                for (let key in json){
+                    let tipo = new clasificacion(json[key].idClasificacion, json[key].nombreClasificacion)
+                    list.push(tipo)
+                }
+                resolve(list)
+            })
+        })
+        dataPromise.then(list => {
+            callback(list)
         })
     },
-    guardar : (req, res) => {
-        let params = JSON.parse(JSON.stringify(req.body))
+
+    guardar : (body) => {
+        let params = JSON.parse(JSON.stringify(body))
         console.log(params)
         dataController.saveNew(params)
-        res.redirect('./')
     }
 }
